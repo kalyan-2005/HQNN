b6f7e0be7d848e839cb28f001ca8619f944c0330
Kalyan Tingani <151056622+kalyan-2005@users.noreply.github.com>
2026-01-22 13:46:05 +0530

    Update input dimension of Linear layer in HybridClassifier

--- hybrid_classifier.py
+++ hybrid_classifier.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 class HybridClassifier(nn.Module):
     def __init__(self, num_classes=4):
         super().__init__()
-        self.fc = nn.Linear(1, num_classes)
+        self.fc = nn.Linear(8, num_classes)
 
     def forward(self, x):
         return self.fc(x)
+
