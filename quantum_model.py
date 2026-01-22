933c0160af95c2facb90e761b8c63735f730e433
Kalyan Tingani <151056622+kalyan-2005@users.noreply.github.com>
2026-01-22 13:47:29 +0530

    Implement multi-observable measurement in QNN
    Added multi-observable measurement functionality to the Quantum Neural Network.

--- quantum_model.py
+++ quantum_model.py
@@ -1,33 +1,43 @@
-# quantum_model.py
+import torch
 import torch.nn as nn
 
 from qiskit.circuit.library import RealAmplitudes
+from qiskit.quantum_info import SparsePauliOp
 from qiskit_machine_learning.neural_networks import EstimatorQNN
 from qiskit_machine_learning.connectors import TorchConnector
 
 
 class QuantumNeuralNetwork(nn.Module):
-    """
-    Trainable Quantum Neural Network using
-    ZZFeatureMap + RealAmplitudes Ansatz
-    """
-
-    def __init__(self, feature_map, n_qubits):
+    def __init__(self, feature_map, num_qubits):
         super().__init__()
 
+        # Variational ansatz
         ansatz = RealAmplitudes(
-            num_qubits=n_qubits,
+            num_qubits=num_qubits,
             reps=1,
-            entanglement='full'
+            entanglement="full"
         )
 
+        circuit = feature_map.compose(ansatz)
+
+        # 🔑 MULTI-OBSERVABLE MEASUREMENT (KEY FIX)
+        observables = []
+
+        for i in range(num_qubits):
+            pauli_str = ["I"] * num_qubits
+            pauli_str[i] = "Z"
+            observables.append(
+                SparsePauliOp("".join(pauli_str))
+            )
+
         qnn = EstimatorQNN(
-            circuit=feature_map.compose(ansatz),
+            circuit=circuit,
+            observables=observables,
             input_params=feature_map.parameters,
             weight_params=ansatz.parameters
         )
 
-        self.qnn_layer = TorchConnector(qnn)
+        self.qnn = TorchConnector(qnn)
 
     def forward(self, x):
-        return self.qnn_layer(x)
+        return self.qnn(x)
